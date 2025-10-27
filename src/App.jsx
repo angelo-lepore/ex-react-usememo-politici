7f95bd023755444d77bd3864e0889e7ab580faf9
angelo-lepore <angelo.lepore94@gmail.com>
2025-10-27 12:07:20 +0100

    milestone-2

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 
 function App() {
   const [politicians, setPoliticians] = useState([]);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     fetch("http://localhost:3333/politicians")
@@ -10,13 +11,31 @@ function App() {
       .catch((error) => console.error(error));
   }, []);
 
+  const filteredPoliticians = useMemo(() => {
+    return politicians.filter((politicians) => {
+      const IsInName = politicians.name
+        .toLowerCase()
+        .includes(search.toLowerCase());
+      const IsInBio = politicians.biography
+        .toLowerCase()
+        .includes(search.toLowerCase());
+      return IsInName || IsInBio;
+    });
+  }, [politicians, search]);
+
   return (
     <>
-      <div>
+      <div className="header-container">
         <h1>Lista politici</h1>
+        <input
+          type="text"
+          placeholder="Cerca politico..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
       </div>
       <div className="politicians-list">
-        {politicians.map((politician) => (
+        {filteredPoliticians.map((politician) => (
           <div className="card" key={politician.id}>
             <img src={politician.image} alt={politician.name} />
             <h2>{politician.name}</h2>
